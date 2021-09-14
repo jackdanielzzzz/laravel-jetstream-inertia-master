05bda1cf8e20c8b7117815d62f4bd279b3aa9669
Jack <40923300+jackdanielzzzz@users.noreply.github.com>
2021-09-14 06:44:04 +0200

    first commit

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -39,7 +39,7 @@
 									<td class="px-6 py-4 whitespace-nowrap">
 										<div class="flex items-center">
 										<div v-if="$page.props.jetstream.managesProfilePhotos" class="flex-shrink-0 h-10 w-10">
-											<img class="h-10 w-10 rounded-full" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name" />
+											<img class="h-10 w-10 rounded-full" :src="user.profile_photo_url" :alt="$page.props.user.name" />
 										</div>
 										<div class="ml-4">
 											<div class="text-sm font-medium text-gray-900">
